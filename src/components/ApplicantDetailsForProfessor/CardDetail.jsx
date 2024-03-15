8b13a8db15763b67525c2d9de786e029d422620c
Roberto Castillo R <38573736+Roberteban@users.noreply.github.com>
2024-03-15 18:48:35 -0300

    Update CardDetail.jsx

--- src/components/ApplicantDetailsForProfessor/CardDetail.jsx
+++ src/components/ApplicantDetailsForProfessor/CardDetail.jsx
@@ -152,13 +152,14 @@ export function CardDetail({
                                     onClick={handleOpenModal('Evaluado Negativamente')}
                                     type="button"
                                     className="w-full sm:w-auto focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                                >Deficiente</button>
+                                >Reprobado</button>
 
                                 <button
                                     onClick={handleOpenModal('Evaluado Positivamente')}
                                     type="button"
                                     className="w-full sm:w-auto focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                                >Excelente</button>
+                                >Aprobado</button>
+
 
 
                                 <Modal
@@ -179,4 +180,5 @@ export function CardDetail({
         </>
     );
 }
+
 
